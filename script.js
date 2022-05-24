655ef9d489d86e3d6ee5309cdb0474cdc0a9f843
Alexandr <Biotman@mail.ru>
2022-05-24 12:31:22 +0300

    Change

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', " ");
 let personalMovieDB = {
     count: numberOfFilms,
@@ -6,10 +8,25 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
-let a = prompt("Один из последних просмотренных фильмов?", ""),
-    b = +prompt("На сколько оцените его?", ""),
-    c = prompt("Один из последних просмотренных фильмов?", ""),
-    d = +prompt("На сколько оцените его?", "");
-personalMovieDB.movies[a]  = b;
-personalMovieDB.movies[c]  = d;
+
+for (let i = 0; i < 2; i++){
+    let a = prompt("Один из последних просмотренных фильмов?", ""),
+        b = prompt("На сколько оцените его?", "");
+        if (a != null && b != null && a != '' && a.length < 50) {
+            personalMovieDB.movies[a]  = b;
+            console.log('Done'); 
+        } else{
+            console.log('error');
+            i--;
+        } 
+}
+if(personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов')
+} else if(personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    console.log("Вы классический зритель");
+} else if(personalMovieDB.count > 30 ){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
 console.log(personalMovieDB);
