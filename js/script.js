93b83041516cc05806b552bef958e724b3a95ff1
Aleksandr Sykhover <Biotman@mail.ru>
2022-08-09 13:45:54 +0300

    Lesson 64 Create Tabs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () =>{
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
     console.log(tabsParent);
     function hideTabContent(){
         tabsContent.forEach(item => {
